915dc6140beb44c5c193b12973fa72e7d3ce78e6
Felipe Passos <berkspar@gmail.com>
2021-02-03 17:06:14 -0300

    fix database issue

--- src/server.js
+++ src/server.js
@@ -1,5 +1,14 @@
 import app from './app';
+import database from './database';
 
 app.listen(process.env.PORT || 3333, () => {
   console.log(`Server listening at port ${process.env.PORT || 3333}`);
+
+  try {
+    database.connection.authenticate().then(() => {
+      console.log('Connection has been established successfully.');
+    });
+  } catch (error) {
+    console.log('Unable to connect to the database:', error);
+  }
 });
